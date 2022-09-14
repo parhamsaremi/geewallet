7e87b0a9a5c78060226bd4e1c7e2896c9bf6edcf
Andres G. Aragoneses <knocte@gmail.com>
2022-09-14 13:38:54 +0800

    WIP restore Mac & iOS projects to fix GitHubCI

---
+++ src/GWallet.Frontend.XF.Mac/Properties/AssemblyInfo.fs
@@ -0,0 +1,14 @@
+﻿namespace GWallet.Frontend.XF.Mac
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("GWallet.Frontend.XF.Mac")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyTrademark("")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
