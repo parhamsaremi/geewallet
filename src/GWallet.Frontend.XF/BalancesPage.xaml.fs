4cc6e81681dcaad419d337e7a3111751c4c2488e
Parham <parhaamsaremi@gmail.com>
2022-09-15 12:54:16 +0430

    WIP: simplify BalancesPage

--- src/GWallet.Frontend.XF/BalancesPage.xaml.fs
+++ src/GWallet.Frontend.XF/BalancesPage.xaml.fs
@@ -13,23 +13,15 @@ type BalancesPage()
 
     let normalChartView = base.FindByName<HoopChartView> "normalChartView"
     let readonlyChartView = base.FindByName<HoopChartView> "readonlyChartView"
-
-    let UpdateLabel (label: Label) =
-        label.Text <- sprintf "%A" 0
     do
         this.Init()
 
     member private this.Init () =
         Device.BeginInvokeOnMainThread(fun _ ->
             normalChartView.SetState()
-
-            UpdateLabel normalChartView.BalanceLabel
-            UpdateLabel readonlyChartView.BalanceLabel
         )
 
         Device.BeginInvokeOnMainThread(fun _ ->
-            UpdateLabel readonlyChartView.BalanceLabel
             readonlyChartView.SetState()
-            UpdateLabel normalChartView.BalanceLabel
             normalChartView.SetState()
         )
