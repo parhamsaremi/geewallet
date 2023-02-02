9a0462de41a7fe8e9f5f87113ba65dbfbe446f39
Parham <parhaamsaremi@gmail.com>
2023-02-02 15:55:01 +0330

    Implement CreateMauiGtkApp

--- src/GWallet.Frontend.Maui/MauiProgram.fs
+++ src/GWallet.Frontend.Maui/MauiProgram.fs
@@ -1,7 +1,17 @@
 ﻿namespace GWallet.Frontend.Maui
 
-open Microsoft.Maui.Hosting
+
+open Gdk
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.DependencyInjection.Extensions
+open Microsoft.Maui
+open Microsoft.Maui.Controls
+open Microsoft.Maui.Controls.Compatibility
+open Microsoft.Maui.Controls.Compatibility.Hosting
 open Microsoft.Maui.Controls.Hosting
+open Microsoft.Maui.Hosting
+open Microsoft.Maui.LifecycleEvents
 
 type MauiProgram =
     static member CreateMauiApp() =
@@ -15,3 +25,24 @@ type MauiProgram =
                 |> ignore
             )
             .Build()
+
+    static member CreateMauiGtkApp() = 
+        let appBuilder = 
+            MauiApp
+                .CreateBuilder()
+                .UseMauiApp<App>()
+                .UseMauiCompatibility()
+        
+        appBuilder
+            .ConfigureFonts(fun fonts ->
+                fonts
+                    .AddFont("OpenSans-Regular.ttf", "OpenSansRegular")
+                    .AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold")
+                |> ignore
+            ) |>ignore
+
+        let services = appBuilder.Services
+        services.AddTransient<IWindow, Window>() |> ignore
+
+        appBuilder.Build()
+        
